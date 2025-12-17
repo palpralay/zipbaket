541537297e85aa587bda18e4c6dc6a3e29734075
Pralay Pal <pralaypal111@gmail.com>
2025-12-17 22:29:34 +0530

    .

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
       <div className="hidden sm:flex items-center gap-8">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/products">All Product</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/seller">Become a Seller</NavLink>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
